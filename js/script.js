a962d8de2dcad0b9e03178ca870d2a728d4a1df7
joao-xarrua <58403926+joao-xarrua@users.noreply.github.com>
2021-03-05 21:36:04 -0300

    Att 1
    Página inicial com menu lateral

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function toggleMenu() {
+    var nav = document.querySelector('.menu');
+    var toggle = document.querySelector('.toggle img');
+    nav.classList.toggle('active');
+    toggle.classList.toggle('active');
+
+}
+
+jQuery(function ($) {
+
+    $('.menu ul li a').click(function () {
+        var a = $(this);
+        var active_tab_class = 'active-tab-menu';
+        var the_tab = '.' + a.attr('data-tab');
+
+        $('.menu ul li a').removeClass(active_tab_class);
+
+        a.addClass(active_tab_class);
+
+        $('.content .tabs').css({
+            'display': 'none'
+        });
+
+        $(the_tab).show();
+
+        return false;
+    });
+});
